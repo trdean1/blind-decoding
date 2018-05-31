f39011f7ddc110780a2697559605793343026603
Tom Dean <trdean@stanford.edu>
2018-05-30 20:36:57 -0700

    Cleaned up old code

--- python/dynamic.py
+++ python/dynamic.py
@@ -29,6 +29,7 @@ def objgrad(x):
     '''
     return np.linalg.inv(x).transpose()
 
+
 def is_feasible(U,Y,tol=1e-14,mask=[]):
     '''
     Returns true if the given value of U is feasible.  Ignores entires
@@ -47,6 +48,7 @@ def is_feasible(U,Y,tol=1e-14,mask=[]):
     else:
         return True
 
+
 def get_active_constraints_bool(U,Y,tol=1e-9):
     '''
     Returns a boolean matrix where each entry tells whether or
@@ -60,26 +62,6 @@ def get_active_constraints_bool(U,Y,tol=1e-9):
     #neg: constraint is met in the negative direction: <u_i, y_j> = -1
     return np.logical_or(pos,neg)
 
-def get_active_constraints_basis(U,Y,tol=1e-9):
-    '''
-    Returns a basis of the constraints that are currently active.
-    A constraint is active if U*Y is within tol of 1.  
-    ''' 
-    c_bool = get_active_constraints_bool(U,Y,tol)
-    (n,k) = c_bool.shape
-     
-    basis = []
-    for i in range(n):
-        for j in range(k):
-            if c_bool[i,j]:
-                row = np.matrix(np.zeros([1,n*n]))
-                row[0,i*n:(i+1)*n] = y[:,j].transpose()
-                if basis == []:
-                    basis = row
-                else:
-                    basis = np.concatenate((basis,row))
-
-    return basis
 
 def random_orthogonal(n):
     '''
@@ -94,45 +76,11 @@ def random_orthogonal(n):
 
     return v.compress(mask,axis=0)
 
+
 def random_unit(n):
     v = np.matrix(np.random.rand( n,1 ) )
     return v / np.linalg.norm(v)
 
-def nullspace(A,eps=1e-12):
-    '''
-    Returns a basis of the null space of the matrix A, with a tolerance given by
-    eps.  I.e., singular vectors whose singular values are less than eps are
-    considered part of the null space
-    '''
-    u,s,vh = scipy.linalg.svd(A)
-    padding = max(0,np.shape(A)[1]-np.shape(s)[0])
-    null_mask = np.concatenate(((s<=eps),np.ones((padding,),dtype=bool)),axis=0)
-    null_space = scipy.compress(null_mask,vh,axis=0)
-    return scipy.transpose(null_space)
-
-
-def project_to_null(v,A):
-    '''
-    projects the vector v onto the nullspace of A
-    '''
-    #These elements of A are all zeros
-    zeros = (np.sum(A,1) == 0)*1
-    if np.sum( zeros ) == 0:
-        #print "Using SVD to find nullspace"
-        #If we have no rows of A that are all zero, find nullspace through SVD
-        null = nullspace(A.transpose())
-        null = np.matrix(null).transpose()
-        rows,cols = null.shape
-        r = np.zeros(v.shape)
-        for row in range(rows):
-            r += (null[row] * v).item() * null[row].transpose()
-    else:
-        #print "Simple zero projection"
-        r = np.multiply( v, zeros )
-
-
-    #print "Projection onto null:\n %s\n" % r
-    return r
 
 def find_infeasible_point(U,V,Y,mask=[]):
     #finds a (non-optimal) value of t so that U+t*V is infeasible
@@ -149,6 +97,7 @@ def find_infeasible_point(U,V,Y,mask=[]):
         raise OverflowError
 
     return t
+
 
 def binary_search(U,V,Y,t_start=0.0,t_stop=[],steps=64,mask=[]):
     #start must be feasible and stop must be infeasible
@@ -171,6 +120,7 @@ def binary_search(U,V,Y,t_start=0.0,t_stop=[],steps=64,mask=[]):
             t -= step
 
     return t
+
 
 def boundary_dist(U, V, Y, uy = [], mask = []): #{{{
     '''
@@ -208,6 +158,7 @@ def boundary_dist(U, V, Y, uy = [], mask = []): #{{{
 
     return t_min
 
+
 def rand_init(n,Y):
     #Generate initial value
     U = random_orthogonal(n)
@@ -218,6 +169,7 @@ def rand_init(n,Y):
         scale = scale*2
 
     return U
+
 
 def row_to_vertex( U, feasible_region, row, UY = [] ):
     '''
@@ -248,56 +200,11 @@ def row_to_vertex( U, feasible_region, row, UY = [] ):
     is_zero_one_list = is_zero_one.tolist()[0] 
     bad_index = [i for i, j in enumerate(is_zero_one_list) if not j ]
 
-    #print UY
-    #print "Trying to fix row %d" % row
-    #print "Bad indices: %s" % bad_index
-
     i = 0
     while not is_zero_one.all():
-        ###############################################################
-        #TODO: Triple check that this loop below will never do anything
-        # row_to_vertex can only get called if the gradient is
-        # in the nullspace of the active constraints, so p will
-        # never be full rank unless something went wrong...
-        #
-        #while True:
-        #    #Check if this has a left null space
-        #    #I actually don't think this code can ever be called
-        #    #
-        #
-        #    # Check if feasbile region has a left nullspace
-        #    if feasible_region.p[row].shape[0] == n:
-        #        #if no nullspace, drop a column with a zero
-        #        is_zero = abs(uY) <= 1e-12
-        #        deleted = False
-        #        for i,bb in enumerate(is_zero):
-        #            if bb:
-        #                #Y_space = np.delete(Y_space, i, axis=1)
-        #                feasible_region.remove( row, i )
-        #                deleted = True
-        #                break
-        #
-        #        if not deleted: 
-        #            #If we have no columns to delete, bail
-        #            raise ValueError
-        #
-        #    #Found a nullspace so we can use it
-        #    else:
-        #        break
-        ################################################################
-
-        Y_space = feasible_region.b[row].T
-        if i >= n:
-            print "\n\nFailed with i=%d, bad=%d" %(i, bad)
-            print bad_index
-            print feasible_region
-            print u*Y
-            raise RuntimeError("This again")
-
         bad = 0
         while i < n:
-            #print "Bad entries: %s" % bad_index
-            #print "i = %d Target %d" % (i, bad_index[bad])
+
             try:
                 y = np.matrix( np.copy( Y[:,bad_index[bad]] ) )
             except IndexError:
@@ -323,13 +230,13 @@ def row_to_vertex( U, feasible_region, row, UY = [] ):
 
                 v = v / np.linalg.norm(v)
 
+            #Force to +1
             t = (1 - np.dot( u, y ) ) / np.dot(v,y)
 
             #this condition implies that we are already at an edge in this
             #direction.  Continue to the next subspace if needed
             if abs(t) < 1e-6:
                 i += 1
-                #print "Already edge in this direction"
                 continue
 
             else:
@@ -337,7 +244,6 @@ def row_to_vertex( U, feasible_region, row, UY = [] ):
 
                 #If u_new is not feasible, try forcing to -1 instead of +1
                 if not is_feasible(u_new,Y,tol=1e-5):
-                    #print "Trying -1"
                     t = (-1 - np.dot( u, y ) ) / np.dot(v,y)
                     u_new = u + t*v 
                     if not is_feasible(u_new,Y,tol=1e-5):
@@ -356,35 +262,24 @@ def row_to_vertex( U, feasible_region, row, UY = [] ):
 
                 #We activated another constraint, save values and leave
                 u = u_new
+
                 i = i + 1
-                #print "Found vertex"
-                #TODO: This code seems to have no function?
                 uY = u * Y
-                #print uY                
                 is_zero_one = check_zero_one( uY )
                 is_zero_one_list = is_zero_one.tolist()[0] 
+
+                #Update the newly active constraints
                 for ii,v in enumerate(is_zero_one_list):
                     if v:
                         feasible_region.insert(row, ii)
+
                 #indices of all the bad entries
                 bad_index = [ii for ii, j in enumerate(is_zero_one_list) if not j ]
-                #print is_zero_one_list
 
-                #TODO: Is break the right call here?
                 break
 
-        #Not sure if we need to do something here...we should be close to \pm 1
-        #if i == n:
-        #   print uY
-        #    print abs(uY) - 1
-        #    raise RuntimeError("This didn't work?")
-
-        if not is_feasible(u, Y,tol=1e-5):
-            print u * Y
-            print feasible_region
-            raise RuntimeError 
-
     return u
+
 
 def check_zero_one(A,eps=1e-6):
     '''
@@ -431,77 +326,6 @@ def find_vertex_on_face(U, Y, feasible_region, UY = []):
 
     return U
 
-def update_p( p, update, Y ):
-    '''
-    For any entry in update that is True, append the appropriate row to p
-    '''
-    (n,k) = Y.shape
-    for i in range(n):
-        for j in range(k):
-            if update[i,j] != True:
-                continue
-            
-            row = np.matrix(np.zeros([1,n*n]))
-            row[0,i*n:(i+1)*n] = Y[:,j].transpose()
-            if p == []:
-                p = row
-            else:
-                p = np.concatenate((p,row))
-
-    return p
-
-def orthogonalize_p(p,tol=1e-9):
-    '''
-    Converts p into an orthogonal basis.  This is basically an optimized QR
-    decomposition
-    '''
-    #This multiply can be made faster since p is sparse
-    #also notice that we update one row of p at time so we should
-    #only need to do a rank one update of A
-    A = p * p.T
-    n = A.shape[0]
-
-    #Find non-zero entires in A, should be easier using a sparse
-    #implementation
-    bad = []
-    for i in range(n):
-        for j in range(n):
-            if j <= i:
-                continue
-            if abs(A[i,j]) > tol:
-                if not ( i in bad ):
-                    bad.append(i)
-                if not ( j in bad ):
-                    bad.append(j)
-
-    #print "Non-orthogonal vectors: %s" % bad
-
-    zero = []
-    #Most of the time, not all vectors overlap all other vectors
-    #so there might be some optimization by not iterating over all pairs 
-    #However, worst case we will have k elements in bad and k(k-1) pairs.
-    #Most commonly we have 2 elements in bad so it doesn't matter
-    for (i,j) in itertools.combinations(bad,2):
-        if (i in zero) or (j in zero):
-            continue
-        #Perform vector rejection
-        u = p[i,:]
-        v = p[j,:]
-        vv = v - ((u*v.T)/(u*u.T))*u
-        if np.linalg.norm(vv) < tol:
-            if not (j in zero):
-                zero.append(j)
-        else:
-            p[j,:] = vv / np.linalg.norm(vv)
-
-    zero.sort()
-    #print "Redundant contraints: %s" % zero
-    p = np.delete(p,zero,0)
-
-    #A = p * p.T
-    #A = np.round(100*A)/100.0
-    #print "p * p.T =\n%s" % A
-    return p
 
 def dynamic_solve(U,Y):
     (n,k) = Y.shape
@@ -548,6 +372,7 @@ def dynamic_solve(U,Y):
 
     return U
 
+
 def rand_zero_one( n, k, use_basis=True ):
     if use_basis:
         X = bases[n]
@@ -558,6 +383,7 @@ def rand_zero_one( n, k, use_basis=True ):
         return X
     else:
         return np.matrix( 2*np.random.randint( 0, 2, size=(n,k) ) - 1 )
+
 
 def trial(n,k):
     #Generate channel and observed samples
